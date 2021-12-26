5d3cf179e895464f0d9ae16bee628f857a984ce5
NimrahAltafAdam <nimrahadam.123@gmail.com>
2021-12-26 05:25:41 -0800

    Widget Lifecycle
    Learned to use initState() and understand the logic behind build function.

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,8 +8,17 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState function run');
+  }
   @override
   Widget build(BuildContext context) {
+    print('build function run');
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -18,7 +27,34 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text('Choose Location')
+      body: RaisedButton(
+        onPressed: () {
+          setState(() {
+            counter += 1;
+          });
+        },
+        child: Text('counter is $counter'),
+      )
     );
   }
 }
+
+//STATELESS
+//State does not chamge over time
+//build function only runs once
+
+//STATEFUL
+//State can change over time
+//setState() triggers the build function
+
+//initState()
+// Called only once when the widget is created
+// Subscribe to streams or any object that could change over widget data
+
+//Build()
+//Build the widget tree
+//a build is triggered everytime we use useState()
+
+//Dispose()
+//When the widget/state object is removed
+
