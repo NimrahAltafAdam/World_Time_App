ca79987d1e2b33bdf93d1e3addc535a25887d1d5
NimrahAltafAdam <nimrahadam.123@gmail.com>
2021-12-26 04:10:51 -0800

    Routing
    Learned to navigate between two screens.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/pages/choose_location.dart';
 //import 'pages/home.dart';
 import 'package:world_time_app/pages/home.dart';
+import 'package:world_time_app/pages/loading.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    initialRoute: '/home',
+    routes: {
+      '/' : (context) => Loading(),
+      '/home' : (context) => Home(),
+      '/location' : (context) => ChooseLocation(),
+    }
   ));
 }
 
