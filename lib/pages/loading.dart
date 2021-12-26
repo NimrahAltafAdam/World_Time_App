7781a83948689df1e16bf65a300e6ac24fcfc58e
NimrahAltafAdam <nimrahadam.123@gmail.com>
2021-12-26 07:10:32 -0800

    World Time Api
    learned how to retrieve data like datetime & offset  of a specific time zone and perform calculations.

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -13,18 +13,36 @@ class _LoadingState extends State<Loading> {
 
   int counter = 0;
 
-  void getData() async {
-    Response response = await get(Uri.http('jsonplaceholder.typicode.com', '/todos/1' ));
+  void getTime() async {
+    /*Response response = await get(Uri.http('jsonplaceholder.typicode.com', '/todos/1' ));
     //use jsonDecode to format the data so that it can be used as an object
     Map data = jsonDecode(response.body);
     print(data);
-    print(data['completed']);
+    print(data['completed']);*/
+    //make the request
+    Response response = await get(Uri.http('worldtimeapi.org', '/api/timezone/Europe/Amsterdam'));
+
+    Map data = jsonDecode(response.body);
+    //print(data);
+
+
+    // get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    // print(datetime);
+    //print(offset);
+
+    //create a datetime object to add offset to datetime
+    // by creating DateTime var we are basically converting the string datetime output from the world clock api into a DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset) ));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
   @override
   Widget build(BuildContext context) {
